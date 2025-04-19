ab1593835d2384cdf6f7923c55ffcf2baa8ee7e3
Jeferson Braine <jefeleal363@gmail.com>
2025-04-19 19:21:46 -0300

    add: how shadowing work in Rust

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,23 @@
+//!using normal let
+
+// fn main() {
+//     let mut x = 5;
+    
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+//! Shadowing
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
 }
