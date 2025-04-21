722dd4a21170f2f11461af4cfb9366d23383176b
Jeferson Braine <jefeleal363@gmail.com>
2025-04-21 14:36:36 -0300

    add: understanding loops

--- loops/target/debug/deps/loops-32150ed31e73ba12.d
+++
@@ -1,5 +0,0 @@
-/home/tenti/Documents/Prog/rust/loops/target/debug/deps/loops-32150ed31e73ba12: src/main.rs
-
-/home/tenti/Documents/Prog/rust/loops/target/debug/deps/loops-32150ed31e73ba12.d: src/main.rs
-
-src/main.rs:
--- loops/target/debug/loops.d
+++
@@ -1,1 +0,0 @@
-/home/tenti/Documents/Prog/rust/loops/target/debug/loops: /home/tenti/Documents/Prog/rust/loops/src/main.rs
